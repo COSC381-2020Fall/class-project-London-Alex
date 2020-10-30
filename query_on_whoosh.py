c528b9640e8ea4276b81ccf5be60be323af10418
Alex London <londoale@umich.edu>
2020-10-30 01:58:14 +0000

    Deploy flask server for query_on_whoosh

--- query_on_whoosh.py
+++ query_on_whoosh.py
@@ -9,9 +9,18 @@ def performQuery(queryTerm, pageNum, itemsPerPage):
         queryParser = QueryParser("description", ix.schema)
         query = queryParser.parse(queryTerm)
         results = searcher.search_page(query, pageNum, pagelen=itemsPerPage)
-        print(results)
+
+        resultsList = []
         for i in results:
-            print(i['title'])
+            resultsDict ={
+                "Title": i['title'],
+                "Description": i['description'],
+                "Link": "https://www.youtube.com/watch?v=%s" % i['id'],
+                "Score": i.score
+            }
+            resultsList.append(resultsDict)
+            
+        return resultsList, results.total, results.pagecount
 
 if __name__ == '__main__':
     queryTerm = sys.argv[1]
